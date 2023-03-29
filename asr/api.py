8f86fb4385fe21fad2fd726770883fac90edc2a0
ArtitSu <62591598+ArtitSu@users.noreply.github.com>
2023-03-29 18:10:00 +0700

    Update api.py

--- asr/api.py
+++ asr/api.py
@@ -8,14 +8,14 @@ from .Wav2vec2WithLM.model import Model, get_model
 app = FastAPI()
 
 
-class SentimentRequest(BaseModel):
+class TranscriptionRequest(BaseModel):
     audio_path: str
 
 
-class SentimentResponse(BaseModel):
+class TranscriptionResponse(BaseModel):
     transcription: str
 
-@app.post("/predict", response_model=SentimentResponse)
-def predict(request: SentimentRequest, model: Model = Depends(get_model)):
+@app.post("/predict", response_model=TranscriptionResponse)
+def predict(request: TranscriptionRequest, model: Model = Depends(get_model)):
     transcription = model.predict(request.audio_path)
-    return SentimentResponse(transcription=transcription)
+    return TranscriptionResponse(transcription=transcription)
